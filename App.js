0f599c2a56a81ec1155396ab180a32d6e53302c4
Grace Miller <miller.gra@northeastern.edu>
2024-04-01 12:53:38 -0400

    completed kanbas section except for one _id error

--- App.js
+++ App.js
@@ -2,11 +2,15 @@ import express from "express";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js"
 import cors from "cors";
+import CourseRoutes from "./Kanbas/Courses/routes.js";
+import ModuleRoutes from "./Kanbas/Modules/routes.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
+ModuleRoutes(app)
+CourseRoutes(app)
 Hello(app)
 Lab5(app)
 
